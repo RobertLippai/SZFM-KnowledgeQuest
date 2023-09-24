9d9931afa57d3fce0584aca3b532812c38d0d82e
Kiss_Gergely <kissgeri01@gmail.com>
2023-09-24 20:31:26 +0200

    Modified questionBankModule.py

--- src/questionBankModule.py
+++ src/questionBankModule.py
@@ -20,13 +20,33 @@ class QuestionBank:
         return random_question
 
     # random question from a given topics
-    def get_random_question_topics(self, topics_name):
-        pass
+    def get_random_question_topics(self, json_file, topics_name):
+        with open(json_file, encoding="utf-8") as f:
+            topics = json.load(f).get("Topics")
+
+            if topics_name not in [topic.get("TopicName") for topic in topics]:
+                raise ValueError("Invalid topic name.")
+
+            for topic in topics:
+                if topic.get("TopicName") == topics_name:
+                    questions = topic.get("Questions")
+                    break
+        random.shuffle(questions)
+        return questions.pop()
 
     # returns all the avaible topics
-    def get_topics(self):
-        return ["Irodalom", "Történelem", "Földrajz"]
+    def get_topics(self, json_file):
+        with open(json_file, encoding="utf-8") as f:
+            topics = json.load(f).get("Topics")
+
+        return [topic.get("TopicName") for topic in topics]
 
     # resets the used question set
     def reset_used_questions(self):
         self.used_questions.clear()
+
+if __name__ == "__main__":
+    question_bank = QuestionBank('test_questions.json')
+    print(question_bank.get_topics("static/questions.json"))
+    print(question_bank.get_random_question())
+    print(question_bank.get_random_question_topics("static/questions.json", "Állatok"))
